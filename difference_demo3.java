55ed04d8bb6631e21cf1c1027d9e896941154cf2
gg12368 <56186550+gg12368@users.noreply.github.com>
2019-11-11 19:33:05 +0800

    Create difference_demo3.java
    全是true，因为标记位没有被清

---
+++ difference_demo3.java
@@ -0,0 +1,30 @@
+public class ThreadDemo {
+	private static class MyRunnable implements Runnable {
+        @Override public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().isInterrupted());
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable target = new MyRunnable();
+        Thread thread = new Thread(target, "李四");
+        thread.start();
+        thread.interrupt();
+    }
+}
+
+
+
+
+运行结果：
+true
+true
+true
+true
+true
+true
+true
+true
+true
+true
